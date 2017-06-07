639b8c9e68e9373d230846dfb026b1eda7f46f76
Gabriel Borba de Oliveira <gabriel.boliveira@yahoo.com.br>
2017-06-07 00:00:50 -0300

    Business validation, services e NLog

---
+++ Rj.SME.Sismonrio.Domain/Contracts/Services/IUsuarioService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rj.SME.Sismonrio.Domain.Contracts.Services
+{
+    public interface IUsuarioService
+    {
+
+    }
+}
